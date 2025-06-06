2a75d6b3e77f7de12f5261190922a68e8524acd0
xnor98 <titus.um.78@gmail.com>
2025-06-06 12:08:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-XAI_API_KEY = "xai-1OXq3qIg8ecSumNwqawHwAJg25Rs3YugtRGzBvqZPKzRc13QPucfQxI99SU4VHE7XP2LnxPVWWyI1pPe"
+XAI_API_KEY = "xai-RpmzdIYF09uIErZ2btAWQrCiv2MdA11WKiS5b6tkNMJd0YsW4MKX5UUyEuJSTd9ra4fykDWqCpVpDKDj"
 
 SUBJECT = input("sujet> ")
 
@@ -40,18 +40,112 @@ def create_books_collection(subject):
         else:
             temp_title += char
 
-    return books 
+    return books, generated_books_titles
 
-def create_chapters_for_books(subject, books):
+def create_chapters_for_books(subject, books, strbooks):
+    print("Génération des chapitres pour chaque livres...")
     books_with_subjects = books
 
     for book in books_with_subjects:
-        prompt="""
-        ### Find 10 chapters to explain the given topic logically, you will only return the chapter titles without numbering them and without saying anything else.
+        print(f"Génération des chapitres pour le livre -> {book}")
 
-        subject : """+subject
+        prompt=f"""
+        You must write between 5 and 10 chapter titles that could fit into the book " {book} " that belongs to the following book collection:
+        {strbooks}
+        The main subject of this book collection is {subject}.
+        The chapter titles must be coherent, you will write only the chapter titles and nothing else. 
+        You will write the number of the chapter like this:
+        1 - replace with chapter name
+        2 - replace with chapter name
+        3 - replace with chapter name
+        etc
+        """
+        completion = client.chat.completions.create(
+            model="grok-3-mini",
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )
+
+        generated_chapters_titles = completion.choices[0].message.content
+
+        temp_title = ""
+        for char in generated_chapters_titles:
+            if char == "\n":
+                books_with_subjects[book].append(temp_title)
+                temp_title = ""
+            else:
+                temp_title += char
+
 
     return books_with_subjects
 
-books = create_books_collection(SUBJECT)
-books_with_subjects = create_chapters_for_books(SUBJECT, books)
+def summary_print(bws):
+    summary = ""
+    for book in bws:
+        summary += "Book: "+ book + "\n"
+        for chap in bws[book]:
+            summary += "     Chapter: "+chap+"\n"
+
+def chapter_content_generator(bws, subject, summary):
+    
+    for book in bws:
+        for chapter in bws[book]:
+
+            prompt = f"""
+            You have to write the contents of the chapter "{chapter}" in the book "{book}".
+            Here is the table of contents of the book collection, I give it to you as a context:
+
+            {summary}
+
+            ### WARNING
+            You must write only the contents of chapter "{chapter}" and not the contents of the other chapters.
+            You must be as detailed as possible in your explanations.
+            You must use markdown format when writing.
+            You'll return the chapter content directly and nothing else.
+            You can use emoji to enhance your content.
+            When writing don't forget the main subject “{subject}”.
+            """
+
+            completion = client.chat.completions.create(
+                model="grok-3-mini",
+                messages=[
+                    {"role": "user", "content": prompt}
+                ]
+            )
+
+            chapter_content = completion.choices[0].message.content
+
+            print("Livre | {book} | Chapitre | {chapter} | Génération terminée !")
+            fn = book+".md"
+            with open(fn, "a") as file:
+                file.write(f"""
+                # {book}
+                ## {chapter}
+
+
+
+                {chapter_content}
+                """)
+            
+
+
+books, strbooks = create_books_collection(SUBJECT)
+books_with_subjects = create_chapters_for_books(SUBJECT, books, strbooks)
+
+print("Tout les titres des livres et des chapitres ont été générés.")
+print("Voulez vous voir le sommaire de votre collection de livres ?")
+rep1 = input("voir le sommaire ? (y/n) > ")
+
+summary = summary_print(books_with_subjects)
+
+
+rep1 = input("voir le sommaire ? (y/n) > ")
+if rep1 == "y":
+    print(summary)
+
+input("(entre pour continuer)")
+
+print("La génération du contenu de chaque chapitre va commencer...")
+
+chapter_content_generator(books_with_subjects, SUBJECT, summary)
